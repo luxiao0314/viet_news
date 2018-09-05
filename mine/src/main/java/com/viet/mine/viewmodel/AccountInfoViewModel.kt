d16420517fee63bbfc16b8f00721f31197992a75
null <zongjia.long@merculet.io>
2018-09-05 11:17:52 +0800

    文字提取到配置文件

--- mine/src/main/java/com/viet/mine/viewmodel/AccountInfoViewModel.kt
+++ mine/src/main/java/com/viet/mine/viewmodel/AccountInfoViewModel.kt
@@ -2,5 +2,6 @@ package com.viet.mine.viewmodel
 
 import com.viet.news.core.viewmodel.BaseViewModel
 
-class PersonalInfoViewModel : BaseViewModel() {
+class AccountInfoViewModel : BaseViewModel() {
+
 }
