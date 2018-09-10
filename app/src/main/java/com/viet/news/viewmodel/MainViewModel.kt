90edfc67c4bb46b8ecca7ad276d24b4580d203b5
Shuqing <shuqing.li@magicwindow.cn>
2018-09-10 17:38:04 +0800

    语言切换

--- app/src/main/java/com/viet/news/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/viet/news/viewmodel/MainViewModel.kt
@@ -13,13 +13,22 @@ import java.util.*
 
 class MainViewModel :BaseViewModel(){
 
-    val titles = mutableListOf(App.instance.getString(R.string.title_find), App.instance.getString(R.string.title_follow), App.instance.getString(R.string.title_task), App.instance.getString(R.string.title_mine))
-    val fragments = mutableListOf(FindFragment(), FollowFragment(), TaskFragment(), MineFragment())
+    var titles = mutableListOf(App.instance.getString(R.string.title_find), App.instance.getString(R.string.title_follow), App.instance.getString(R.string.title_task), App.instance.getString(R.string.title_mine))
+    var fragments = mutableListOf(FindFragment(), FollowFragment(), TaskFragment(), MineFragment())
     private val mIconUnselectIds = arrayOf(R.drawable.ic_find_unselect, R.drawable.ic_follow_unselect, R.drawable.ic_task_unselect, R.drawable.ic_mine_unselect)
     private val mIconSelectIds = arrayOf(R.drawable.ic_find_select, R.drawable.ic_follow_select, R.drawable.ic_task_select, R.drawable.ic_mine_select)
     val tabEntities = ArrayList<CustomTabEntity>()
 
     init {
+        for (i in titles.indices) {
+            tabEntities.add(TabEntity(titles[i], mIconSelectIds[i], mIconUnselectIds[i]))
+        }
+    }
+
+    fun reLoadData(){
+        titles = mutableListOf(App.instance.getString(R.string.title_find), App.instance.getString(R.string.title_follow), App.instance.getString(R.string.title_task), App.instance.getString(R.string.title_mine))
+        fragments = mutableListOf(FindFragment(), FollowFragment(), TaskFragment(), MineFragment())
+        tabEntities.clear()
         for (i in titles.indices) {
             tabEntities.add(TabEntity(titles[i], mIconSelectIds[i], mIconUnselectIds[i]))
         }
--- task/src/main/java/com/viet/task/fragment/TaskFragment.kt
+++ task/src/main/java/com/viet/task/fragment/TaskFragment.kt
@@ -1,18 +1,28 @@
 package com.viet.task.fragment
 
 import android.arch.lifecycle.Observer
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.OrientationHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.safframework.log.L
 import com.viet.news.core.delegate.viewModelDelegate
+import com.viet.news.core.ext.toast
 import com.viet.news.core.ui.BaseFragment
 import com.viet.task.R
 import com.viet.task.adapter.TaskAdapter
 import com.viet.task.viewmodel.TaskViewModel
 import kotlinx.android.synthetic.main.fragment_task.*
+
 
 /**
  * @Description 任务
@@ -27,6 +37,8 @@ class TaskFragment : BaseFragment() {
 
     private val adapter: TaskAdapter = TaskAdapter()
 
+    private val callbackManager: CallbackManager = CallbackManager.Factory.create()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_task, container, false)
     }
@@ -35,6 +47,45 @@ class TaskFragment : BaseFragment() {
         recycler_view_task.adapter = adapter
         recycler_view_task.layoutManager = LinearLayoutManager(activity, OrientationHelper.VERTICAL, false)
         initData()
+
+        LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                result?.let {
+                    it.accessToken.toString()
+                    L.e("onSuccess,userId = ${it.accessToken.userId}" +
+                            ",token = ${it.accessToken.token}" +
+                            ",applicationId = ${it.accessToken.applicationId}" +
+                            ",isExpired = ${it.accessToken.isExpired}")
+                    toast("onSuccess,result = ${it.accessToken}").show()
+                }
+            }
+
+            override fun onCancel() {
+                L.e("onCancel")
+                toast("onCancel").show()
+            }
+
+            override fun onError(error: FacebookException?) {
+                L.e("onError , error=$error")
+                toast("onError , error=$error").show()
+            }
+        })
+
+        btn_test.setOnClickListener {
+            val accessToken = AccessToken.getCurrentAccessToken()
+            val isLoggedIn = accessToken != null && !accessToken.isExpired
+            if (isLoggedIn) {
+                LoginManager.getInstance().logOut()
+            } else {
+                LoginManager.getInstance().logInWithReadPermissions(this, listOf("public_profile"))
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun initData() {
