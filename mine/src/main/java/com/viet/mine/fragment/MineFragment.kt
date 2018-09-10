94986e672a2242a4f89c3b0ad3b8d44a8b5025e2
null <zongjia.long@merculet.io>
2018-09-10 14:46:03 +0800

    登录状态判断

--- mine/src/main/java/com/viet/mine/fragment/MineFragment.kt
+++ mine/src/main/java/com/viet/mine/fragment/MineFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.safframework.ext.clickWithTrigger
+import com.viet.follow.activity.PersonalPageActivity
 import com.viet.mine.R
 import com.viet.mine.activity.*
 import com.viet.news.core.domain.User
@@ -31,41 +32,57 @@ class MineFragment : BaseFragment() {
     }
 
     override fun initView(view: View) {
-        iv_user_icon.loadCircle("")
-        btn_login.clickWithTrigger { context?.startActivity(Intent(activity, LoginActivity::class.java)) }
-        mine_wallet.clickWithTrigger { WebActivity.launch(context, "http://www.baidu.com") }
-        mine_settings.setClickDelegate {
-            onItemClick = { context?.startActivity(Intent(activity, SettingActivity::class.java)) }
+        if (!User.currentUser.isLogin()) {
+            btn_login.visibility = View.VISIBLE
+            iv_user_icon.visibility = View.GONE
+            rl_user.visibility = View.GONE
+            edit.visibility = View.GONE
+            btn_login.clickWithTrigger { context?.startActivity(Intent(activity, LoginActivity::class.java)) }
+        } else {
+            btn_login.visibility = View.GONE
+            iv_user_icon.visibility = View.VISIBLE
+            rl_user.visibility = View.VISIBLE
+            edit.visibility = View.VISIBLE
+            iv_user_icon.loadCircle("")
+            iv_user_icon.clickWithTrigger {
+                startActivity(Intent(activity, PersonalPageActivity::class.java))
+            }
+            edit.clickWithTrigger {
+                context?.startActivity(Intent(activity, AccountInfoActivity::class.java))
+            }
         }
 
-        iv_user_icon.clickWithTrigger {
-            if (!User.currentUser.isLogin()) {
-                startActivityForResult(Intent(activity, LoginActivity::class.java), 0)
-            } else {
-                startActivity(Intent(activity, AccountInfoActivity::class.java))
+        mine_settings.setClickDelegate {
+            onItemClick = {
+                startActivity(SettingActivity::class.java)
             }
         }
 
         mine_collection.setClickDelegate {
             onItemClick = {
-                context?.startActivity(Intent(activity, CollectionActivity::class.java))
+                startActivity(CollectionActivity::class.java)
             }
-        }
-
-        edit.clickWithTrigger {
-            context?.startActivity(Intent(activity, AccountInfoActivity::class.java))
         }
 
         mine_invite.setClickDelegate {
             onItemClick = {
-                context?.startActivity(Intent(activity, InviteFriendActivity::class.java))
+                startActivity(InviteFriendActivity::class.java)
             }
         }
 
         mine_wallet.setClickDelegate {
             onItemClick = {
-                context?.startActivity(Intent(activity, MineWalletActivity::class.java))
+                startActivity(MineWalletActivity::class.java)
             }
+        }
+
+    }
+
+    private fun startActivity(cls: Class<*>) {
+        if (!User.currentUser.isLogin()) {
+            context?.startActivity(Intent(activity, LoginActivity::class.java))
+        } else {
+            context?.startActivity(Intent(activity, cls))
         }
     }
 }
