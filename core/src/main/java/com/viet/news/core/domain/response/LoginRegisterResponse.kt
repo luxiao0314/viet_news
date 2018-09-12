14339cd4da645654709a385b5db4168da07d32a8
null <zongjia.long@merculet.io>
2018-09-12 11:15:34 +0800

    User字段添加

--- core/src/main/java/com/viet/news/core/domain/response/LoginRegisterResponse.kt
+++ core/src/main/java/com/viet/news/core/domain/response/LoginRegisterResponse.kt
@@ -5,9 +5,11 @@ import com.viet.news.core.api.HttpResponse
 
 class LoginRegisterResponse : HttpResponse<LoginRegisterResponse.LoginRegister>() {
 
-     data class LoginRegister(
+    data class LoginRegister(
+            @SerializedName("fans_count") var fansCount: Int = 0,
             @SerializedName("user_id") var userId: String = "",
             @SerializedName("role_id") var roleId: String = "",
+            @SerializedName("phone_number") var followCount: Int = 0,
             @SerializedName("nick_name") var nickName: String = "",
             @SerializedName("phone_number") var phoneNumber: String = "",
             @SerializedName("token") var token: String = "",
