3719ea6d84fa791524c928397e149ed8fc55fe11
null <zongjia.long@merculet.io>
2018-09-14 19:19:52 +0800

    重置手机号页面添加

--- core/src/main/java/com/viet/news/core/config/Config.kt
+++ core/src/main/java/com/viet/news/core/config/Config.kt
@@ -65,6 +65,7 @@ object Config {
     const val ROUTER_MINE_EDIT_INFO_FRAGMENT = "/mine/edit_info/fragment"
     const val ROUTER_MINE_EDIT_CHANGE_NICKNAME_FRAGMENT = "/mine/edit/change/nickname/fragment"
     const val ROUTER_MINE_EDIT_CHANGE_PHONE_FRAGMENT = "/mine/edit/change/phone/fragment"
+    const val ROUTER_MINE_EDIT_BIND_PHONE_FRAGMENT = "/mine/edit/bind/phone/fragment"
     const val ROUTER_MINE_EDIT_CHANGE_PWD_FRAGMENT = "/mine/edit/change/pwd/fragment"
     const val ROUTER_MINE_EDIT_VERIFY_CODE_FRAGMENT = "/mine/edit/verify_code/fragment"
 
