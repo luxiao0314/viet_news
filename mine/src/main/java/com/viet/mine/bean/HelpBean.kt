3b6b52bb969d1fe2868b819add4027026cc92261
null <zongjia.long@merculet.io>
2018-09-19 14:25:18 +0800

    帮助页面改为可扩展

---
+++ mine/src/main/java/com/viet/mine/bean/HelpBean.kt
@@ -0,0 +1,7 @@
+package com.viet.mine.bean
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class HelpBean(@SerializedName("name")
+                    var name: String )
