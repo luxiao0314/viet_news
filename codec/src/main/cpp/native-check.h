5a70545c2c8c7595c948372d21f8f24ebcff5560
Shuqing <shuqing.li@magicwindow.cn>
2018-09-19 16:23:45 +0800

    密码加密

---
+++ codec/src/main/cpp/native-check.h
@@ -0,0 +1,20 @@
+//
+// Created by Tony on 2018/5/22.
+//
+#include <jni.h>
+
+#ifndef MAGICWALLET_ANDROID_NATIVE_CHECK_H
+#define MAGICWALLET_ANDROID_NATIVE_CHECK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT jstring JNICALL
+Java_com_viet_news_core_utils_EncryptUtils_nativeCheck(JNIEnv *env, jclass type);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- core/src/main/java/com/viet/news/core/domain/request/ResetPwdWithOldpwdParams.kt
+++ core/src/main/java/com/viet/news/core/domain/request/ResetPwdWithOldpwdParams.kt
@@ -1,6 +1,14 @@
 package com.viet.news.core.domain.request
 
+import cn.magicwindow.core.utils.EncryptUtils
+
 class ResetPwdWithOldpwdParams{
     var new_password: String? = null
+        set(value) {
+            field = EncryptUtils.encryptPayPassword(value?:"")
+        }
     var old_password: String? = null
+        set(value) {
+            field = EncryptUtils.encryptPayPassword(value?:"")
+        }
 }
